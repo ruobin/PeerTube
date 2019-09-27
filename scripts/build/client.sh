643c1c501c7231511f6650649a7fa2bbe8ad76f7
Wang, Ruobin <Ruobin.Wang@disney.com>
2019-09-27 19:43:06 +0800

    only keep simplified Chinese and English languages

--- scripts/build/client.sh
+++ scripts/build/client.sh
@@ -42,7 +42,7 @@ if [ -z ${1+x} ] || [ "$1" != "--light" ]; then
     else
         # Supported languages
         languages=(
-            # "zh_Hans_CN"
+            "zh_Hans_CN"
             # "fi_FI" "nl_NL" "gd" "el_GR" "es_ES" "oc" "pt_BR" "pt_PT" "sv_SE" "pl_PL" "ru_RU" "zh_Hans_CN" "zh_Hant_TW"
             # "fr_FR" "ja_JP" "eu_ES" "ca_ES" "cs_CZ" "eo" "de_DE" "it_IT"
         )
