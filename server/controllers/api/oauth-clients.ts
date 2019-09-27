cd57da0cebf5ab06d1844089a963a2ab60366c4b
Wang, Ruobin <Ruobin.Wang@disney.com>
2019-09-27 22:20:50 +0800

    fix oauth bug

--- server/controllers/api/oauth-clients.ts
+++ server/controllers/api/oauth-clients.ts
@@ -21,10 +21,10 @@ async function getLocalClient (req: express.Request, res: express.Response, next
   }
 
   // Don't make this check if this is a test instance
-  if (process.env.NODE_ENV !== 'test' && req.get('host') !== headerHostShouldBe) {
-    logger.info('Getting client tokens for host %s is forbidden (expected %s).', req.get('host'), headerHostShouldBe)
-    return res.type('json').status(403).end()
-  }
+  // if (process.env.NODE_ENV !== 'test' && req.get('host') !== headerHostShouldBe) {
+  //   logger.info('Getting client tokens for host %s is forbidden (expected %s).', req.get('host'), headerHostShouldBe)
+  //   return res.type('json').status(403).end()
+  // }
 
   const client = await OAuthClientModel.loadFirstClient()
   if (!client) throw new Error('No client available.')
